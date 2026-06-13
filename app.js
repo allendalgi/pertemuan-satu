143dc2f865d7ccf5dee035afd6f024bcaeba5963
allendalgi <allendalgi07@gmail.com>
2026-06-13 17:02:02 +0700

    Update

---
+++ app.js
@@ -0,0 +1,32 @@
+// Operasi Aritmatika
+let a = 10
+let b = 5
+
+a = 10
+b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)
+
+
+// Operasi Perbandingan
+let nilai = 80;
+
+console.log(nilai > 70);
+console.log(nilai < 70);
+console.log(nilai === "80");
+console.log(nilai !== 90);
+
+
+// operation logika
+let usernamebenar = true;
+let passwordbenar = true;
+
+console.log(usernamebenar && passwordbenar);
+console.log( usernamebenar || passwordbenar);
+console.log(usernamebenar)
+
